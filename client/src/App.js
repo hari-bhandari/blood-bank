9026b0cec6472567814b1d3d48531d4f23fd326b
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-11-07 16:43:55 +0000

    added auth context from previos pj

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,19 @@ import './App.css';
 import Auth from "./Components/Auth/Auth";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import AuthState from "./Context/auth/AuthState";
 function App() {
   return (
     <div className="App">
+        <AuthState>
         <Router>
             <Navbar />
             <Switch>
                 <Route path='/login' exact component={Auth} />
             </Switch>
         </Router>
+        </AuthState>
 
-      <Auth/>
     </div>
   );
 }

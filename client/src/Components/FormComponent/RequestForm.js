587f52dd68caf96bbb7118d2766b57108c215150
Hari Bhandari <2012bhandari.ha@gmail.com>
2020-11-09 03:13:12 +0000

    added blood request form

---
+++ client/src/Components/FormComponent/RequestForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './RequestForm.css'
+const RequestForm = () => {
+    return (
+        <div className="card-form">
+            <form className="signup">
+                <div className="form-title">Sign Up for our Newsletter!</div>
+                <div className="form-body">
+                    <div className="row">
+                        <input type="text" placeholder="Name" required/>
+                            <input type="text" placeholder="Blood Type"/>
+                    </div>
+                    <div className="row">
+                        <input type="text" placeholder="City" required/>
+                        <input type="text" placeholder="Hospital Name"/>
+                    </div>
+                    <div className="row">
+                        <input type="text" placeholder="Email Address*"/>
+                    </div>
+                    <div className="row">
+                        <input type="text" placeholder="Phone Number"/>
+                    </div>
+                </div>
+                <div className="rule"></div>
+                <div className="form-footer">
+                    <a>Sign Me Up!<span className="fa fa-thumbs-o-up"></span></a>
+                    <a>Not Now!<span className="fa fa-ban"></span></a>
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default RequestForm;
--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -2,18 +2,22 @@ import React from 'react';
 import DonorCard from "./DonorCard";
 import './Homepage.css'
 import { Container, Row, Col } from 'react-grid-system';
+import {NavBtnLink} from "./Navbar/NavbarElements";
+import RequestForm from "./FormComponent/RequestForm";
 const HomePage = () => {
     return (
         <Container >
             <Row>
-                <Col sm={9}>
-                    One of three columns
+                <Col lg={9}>
+                    <h3>Request  for a blood</h3>
+                    <RequestForm/>
                 </Col>
-                <Col sm={3}>
+                <Col lg={3}>
                     <h3>Need Action immediately</h3>
                    <DonorCard/>
                    <DonorCard/>
                    <DonorCard/>
+                    <NavBtnLink to='/help' style={{marginTop:'10px'}}>See More</NavBtnLink>
                 </Col>
             </Row>
         </Container>

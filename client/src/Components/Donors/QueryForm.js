06d0fda122b0f2bd1e0c807d7d296d4c53ac7661
Your Name <you@example.com>
2020-12-11 19:01:27 +0000

    added choose blood type option

--- client/src/Components/Donors/QueryForm.js
+++ client/src/Components/Donors/QueryForm.js
@@ -1,5 +1,5 @@
 import React,{useEffect,useState} from 'react';
-import {QueryContainer} from "./QueryFormCss";
+import {QueryContainer, SelectBox} from "./QueryFormCss";
 import Select from "react-select";
 import axios from "axios";
 const QueryForm = () => {
@@ -7,6 +7,11 @@ const QueryForm = () => {
     const handleChange = selectedOption => {
         console.log(`Option selected:`, selectedOption);
     };
+    const bloodTypes=['A+','A-','B+','B-','O+','O-','AB+',]
+    const bloodOptions=bloodTypes.map(d=>({
+        "value":d,
+        "label":d.charAt(0).toUpperCase() + d.slice(1)
+    }))
     const loadValue=async ()=>{
         try{
             const data=await axios.get('https://api.npoint.io/8af3c8b340e081e55551')
@@ -24,13 +29,22 @@ const QueryForm = () => {
     },[])
     return (
         <QueryContainer>
-
-            Blood Type
+            <SelectBox>
+            Choose District
             <Select
                 onChange={handleChange}
                 options={options}
                 isMulti
             />
+            </SelectBox>
+            <SelectBox>
+                Choose Blood Type
+                <Select
+                    onChange={handleChange}
+                    options={bloodOptions}
+                    isMulti
+                />
+            </SelectBox>
         </QueryContainer>
     );
 };
--- client/src/Components/Donors/QueryFormCss.js
+++ client/src/Components/Donors/QueryFormCss.js
@@ -2,15 +2,21 @@ import styled from "styled-components";
 export const QueryContainer=styled.div`
         width: 100%;
     display: flex;
+  
+  
   .css-2b097c-container{
-    width: 30%;
+    width: 100%;
   }
   @media screen and (max-width: 700px) {
     .css-2b097c-container{
-      width: 50%;
+      width: 100%;
     }
+  }
+  .p{
+    margin: auto 0px;
   }
 `
 export const SelectBox=styled.div`
-  margin:auto 0px
+    width: 100%;
+  flex: 0.5;
     `

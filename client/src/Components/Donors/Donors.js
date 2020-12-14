3205314cc426d272ba814817738b8955d2ab7c62
Your Name <you@example.com>
2020-12-14 21:33:06 +0000

    implemented query

--- client/src/Components/Donors/Donors.js
+++ client/src/Components/Donors/Donors.js
@@ -5,13 +5,15 @@ import QueryForm from "./QueryForm";
 import BloodContext from "../../Context/blood/bloodContext";
 import SelectComponent from "../query/SelectComponent";
 import {bloodType,districts,turnIntoSelectFormat} from "../utils/sharedData";
+import axios from "axios";
+import {useQuery} from "react-query";
 
 const Donors = () => {
-    const bloodContext=useContext(BloodContext)
-    const {getDonors,donors,loading}=bloodContext
-    useEffect(()=>{
-        getDonors()
-    },[])
+    const { isLoading, error, data } = useQuery('donors', () =>
+        axios('/api/donors').then(res =>
+            res.data.data
+        )
+    )
     const districtOptions=turnIntoSelectFormat(districts)
     const bloodOptions=turnIntoSelectFormat(bloodType)
     const handleChange = selectedOption => {
@@ -26,7 +28,7 @@ const Donors = () => {
                 <Col lg={6} md={6} sm={12}>
                     <SelectComponent options={districtOptions} isMulti={false} defaultLabel={"Query by District..."} onChange={handleChange}/>
                 </Col>
-                {donors?.map(donor=>(
+                {data?.map(donor=>(
                     <Col lg={4} md={6} sm={12}>
                         <DonorItem donor={donor}/>
                     </Col>

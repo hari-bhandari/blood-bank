aa7b0ad6fb4b2cdb1be6fcae2da2ffc3a1047c15
Your Name <you@example.com>
2020-12-14 21:51:30 +0000

    minor chanegs

--- client/src/Components/Donors/QueryForm.js
+++ client/src/Components/Donors/QueryForm.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import {QueryContainer} from "./QueryFormCss";
 import SelectComponent from "../query/SelectComponent";
-import {districts,bloodType,turnIntoSelectFormat} from "../utils/sharedData";
+import {districts,bloodType} from "../utils/sharedData";
 
 const QueryForm = () => {
-    const Districts=turnIntoSelectFormat(districts)
-    const BloodTypes=turnIntoSelectFormat(bloodType)
-    const handleChange = selectedOption => {
-        console.log(`Option selected:`, selectedOption);
-    };
+
     return (
         <QueryContainer>
-         <SelectComponent onChange={handleChange} defaultLabel={"Blood"} isMulti={false} options={BloodTypes}/>
+         {/*<SelectComponent onChange={handleChange} defaultLabel={"Blood"} isMulti={false} options={BloodTypes}/>*/}
         </QueryContainer>
     );
 };

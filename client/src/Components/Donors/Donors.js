75f1f6b776aab62ba17b33b2a85b3363e550cf99
Your Name <you@example.com>
2020-12-14 19:06:06 +0000

    made select different component

--- client/src/Components/Donors/Donors.js
+++ client/src/Components/Donors/Donors.js
@@ -3,6 +3,8 @@ import DonorItem from "./DonorItem";
 import {Container, Row, Col} from 'react-grid-system'
 import QueryForm from "./QueryForm";
 import BloodContext from "../../Context/blood/bloodContext";
+import SelectComponent from "../query/SelectComponent";
+import {bloodType,districts,turnIntoSelectFormat} from "../utils/sharedData";
 
 const Donors = () => {
     const bloodContext=useContext(BloodContext)
@@ -10,13 +12,22 @@ const Donors = () => {
     useEffect(()=>{
         getDonors()
     },[])
-
+    const districtOptions=turnIntoSelectFormat(districts)
+    const bloodOptions=turnIntoSelectFormat(bloodType)
+    const handleChange = selectedOption => {
+        console.log(`Option selected:`, selectedOption);
+    };
     return (
         <Container>
-            <QueryForm/>
             <Row>
+                <Col lg={6} md={6} sm={12}>
+                    <SelectComponent options={bloodOptions} isMulti={false} defaultLabel={"Query Blood Type..."} onChange={handleChange}/>
+                </Col>
+                <Col lg={6} md={6} sm={12}>
+                    <SelectComponent options={districtOptions} isMulti={false} defaultLabel={"Query by District..."} onChange={handleChange}/>
+                </Col>
                 {donors?.map(donor=>(
-                    <Col lg={4} md={6} sm={1}>
+                    <Col lg={4} md={6} sm={12}>
                         <DonorItem donor={donor}/>
                     </Col>
                 ))}
--- client/src/Components/Donors/QueryForm.js
+++ client/src/Components/Donors/QueryForm.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import {QueryContainer} from "./QueryFormCss";
 import SelectComponent from "../query/SelectComponent";
-import {districts,bloodType} from "../utils/sharedData";
+import {districts,bloodType,turnIntoSelectFormat} from "../utils/sharedData";
 
 const QueryForm = () => {
-    const bloodOptions=bloodType.map(d=>({
-        "value":d,
-        "label":d.charAt(0).toUpperCase() + d.slice(1)
-    }))
+    const Districts=turnIntoSelectFormat(districts)
+    const BloodTypes=turnIntoSelectFormat(bloodType)
     const handleChange = selectedOption => {
         console.log(`Option selected:`, selectedOption);
     };
     return (
         <QueryContainer>
-         <SelectComponent onChange={handleChange} defaultLabel={"Blood"} isMulti={false} options={bloodOptions}/>
+         <SelectComponent onChange={handleChange} defaultLabel={"Blood"} isMulti={false} options={BloodTypes}/>
         </QueryContainer>
     );
 };

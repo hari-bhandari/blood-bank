e5f3dcee6e431bae93629ee93ab1c5873e66d6d8
Your Name <you@example.com>
2020-12-15 17:14:45 +0000

    added spinner

--- client/src/Components/Donors/Donors.js
+++ client/src/Components/Donors/Donors.js
@@ -5,37 +5,19 @@ import SelectComponent from "../query/SelectComponent";
 import {bloodType,districts,turnIntoSelectFormat} from "../utils/sharedData";
 import axios from "axios";
 import {useQuery} from "react-query";
+import {SpinnerInfinity} from "spinners-react";
 
 const Donors = () => {
 
     const [district,setDistrict]=useState(null)
     const [blood,setBlood]=useState(null)
     const fetchDonors = async () => {
-        let response
-        if(district!==null && blood!==null){
-            response = await axios(
-                `/api/donors?bloodType=${blood}&district=${district}`
-            );
-        }
-        if(district===null && blood==null){
-            response = await axios(
-                `/api/donors`
-            );
-        }
-        if(district===!null || blood==null){
-            response = await axios(
-                `/api/donors?district=${district}`
-            );
-        }
-
-        if(district===null || blood!==null){
-            response = await axios(
-                `/api/donors?bloodType=${blood}`
-            );
-        }
+        const response = await axios(
+            `/api/donors?bloodType=${blood}&district=${district}`
+        );
         return response.data.data;
-    };
-    const { status, data } = useQuery([district,blood], fetchDonors, {
+    }
+    let { status, data } = useQuery([district,blood], fetchDonors, {
         refetchAllOnWindowFocus: false
     });
     const districtOptions=turnIntoSelectFormat(districts)
@@ -46,14 +28,21 @@ const Donors = () => {
     const handleChangeForBlood = selectedOption => {
         setBlood(encodeURIComponent(selectedOption.value))
     };
+    status="loading"
+    if(status==="loading"){
+        return(
+
+            <SpinnerInfinity size={90} thickness={100} speed={100} color="#36ad47" secondaryColor="rgba(0, 0, 0, 0.44)" />
+        )
+    }
     return (
         <Container>
             <Row>
                 <Col lg={6} md={6} sm={12}>
-                    <SelectComponent options={bloodOptions} isMulti={false}  onChange={handleChangeForBlood}/>
+                    <SelectComponent options={bloodOptions} isMulti={false}  onChange={handleChangeForBlood} defaultLabel={"Search by blood type..."}/>
                 </Col>
                 <Col lg={6} md={6} sm={12}>
-                    <SelectComponent options={districtOptions} isMulti={false}  onChange={handleChangeForDistrict} defaultLabel={"Choose District"}/>
+                    <SelectComponent options={districtOptions} isMulti={false}  onChange={handleChangeForDistrict} defaultLabel={"Search by district..."}/>
                 </Col>
                 {data?.map(donor=>(
                     <Col lg={4} md={6} sm={12}>

51c92c476ec6db4c9123ff1503814e8c6e6120c5
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-05 06:25:28 +0000

    added .

--- client/src/Components/pages/UploadPhoto.js
+++ client/src/Components/pages/UploadPhoto.js
@@ -1,26 +1,11 @@
 import 'react-dropzone-uploader/dist/styles.css'
 import Dropzone from 'react-dropzone-uploader'
+import ImageUploader from 'react-images-upload'
+import axios from "axios";
 
-const UploadPhoto = () => {
-    // specify upload params and url for your files
-    const getUploadParams = ({ meta }) => { return { url: 'https://httpbin.org/post' } }
-
-    // called every time a file's `status` changes
-    const handleChangeStatus = ({ meta, file }, status) => { console.log(status, meta, file) }
-
-    // receives array of files that are done uploading when submit button is clicked
-    const handleSubmit = (files, allFiles) => {
-        console.log(files.map(f => f.meta))
-        allFiles.forEach(f => f.remove())
-    }
-
+const UploadPhoto = (props) => {
     return (
-        <Dropzone
-            getUploadParams={getUploadParams}
-            onChangeStatus={handleChangeStatus}
-            onSubmit={handleSubmit}
-            accept="image/*"
-        />
+        <ImageUploader {...props} withIcon={false} withPreview={true}/>
     )
 }
 export default UploadPhoto

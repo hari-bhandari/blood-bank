db7e6d41893b2b5b7557e94da9dd16dd8579d70c
hari-bhandari <2012bhandari.ha@gmail.com>
2021-01-08 20:12:10 +0000

    fig bug when image name is invalid

--- client/src/Components/sharedUtils/utils.js
+++ client/src/Components/sharedUtils/utils.js
@@ -1,6 +1,10 @@
 export const getSizedImageURL=(imageName,width,height)=>{
-    const re = /(?:\.([^.]+))?$/;
-    const extension=re.exec(imageName)[0]
-    const ImageInitialName=imageName.replace(extension,'')
-    return `/uploads/${ImageInitialName}_${width}x${height}${extension}`
+    try {
+        const re = /(?:\.([^.]+))?$/;
+        const extension = re.exec(imageName)[0]
+        const ImageInitialName = imageName.replace(extension, '')
+        return `/uploads/${ImageInitialName}_${width}x${height}${extension}`
+    }catch (e){
+        return `/uploads/noProfile.png`
+    }
 }
